95340fe3369bc823d0b5de0fd0336495ee5d325b
gwandubruce <62942199+gwandubruce@users.noreply.github.com>
2021-01-28 19:25:37 +0200

    Create InfectionCheckViewModel.java

---
+++ app/src/main/java/app/bandemic/viewmodel/InfectionCheckViewModel.java
@@ -0,0 +1,33 @@
+package app.bandemic.viewmodel;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import app.bandemic.strict.database.Infection;
+import app.bandemic.strict.repository.InfectedUUIDRepository;
+
+public class InfectionCheckViewModel extends AndroidViewModel {
+
+    private final InfectedUUIDRepository mRepository;
+
+    private final LiveData<List<Infection>> possiblyInfectedEncounters;
+
+    public InfectionCheckViewModel(Application application) {
+        super(application);
+        mRepository = new InfectedUUIDRepository(application);
+        possiblyInfectedEncounters = mRepository.getPossiblyInfectedEncounters();
+    }
+
+    public void refreshInfectedUUIDs() {
+        mRepository.refreshInfectedUUIDs();
+    }
+
+    public LiveData<List<Infection>> getPossiblyInfectedEncounters() {
+
+        return possiblyInfectedEncounters; }
+
+}
