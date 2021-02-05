4eb3f4b0010b902dd7591ab93dd58de5f1b303d3
gwandubruce <62942199+gwandubruce@users.noreply.github.com>
2021-02-05 20:15:53 +0200

    Update InfectedUUIDResponse.java

--- app/src/main/java/app/bandemic/strict/network/InfectedUUIDResponse.java
+++ app/src/main/java/app/bandemic/strict/network/InfectedUUIDResponse.java
@@ -1,10 +1,19 @@
 package app.bandemic.strict.network;
 
+import com.google.firebase.database.IgnoreExtraProperties;
+
 import java.util.List;
 
 import app.bandemic.strict.database.InfectedUUID;
-
+@IgnoreExtraProperties
 public class InfectedUUIDResponse {
+    public InfectedUUIDResponse() {
+    }
+
+    public InfectedUUIDResponse(List<InfectedUUID> data) {
+        this.data = data;
+    }
+
     public List<InfectedUUID> data;
 
     public List<InfectedUUID> getData() {
