76e581ea765aacdab469ca7fd9a877f9d8ffe90a
gwandubruce <62942199+gwandubruce@users.noreply.github.com>
2021-02-05 20:14:09 +0200

    Update OwnUUIDResponse.java

--- app/src/main/java/app/bandemic/strict/network/OwnUUIDResponse.java
+++ app/src/main/java/app/bandemic/strict/network/OwnUUIDResponse.java
@@ -1,11 +1,21 @@
 package app.bandemic.strict.network;
 
+import com.google.firebase.database.IgnoreExtraProperties;
+
 import java.util.List;
 
 import app.bandemic.strict.database.OwnUUID;
-
+@IgnoreExtraProperties
 public class OwnUUIDResponse {
+    public OwnUUIDResponse() {
+    }
+
+    public OwnUUIDResponse(List<OwnUUID> data) {
+        this.data = data;
+    }
+
     public List<OwnUUID> data;
+
 
     public List<OwnUUID> getData() {
         return data;
