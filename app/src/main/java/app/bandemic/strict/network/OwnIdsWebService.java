c125a7696bbc154da49e55e6c24b4c8df3c0045e
gwandubruce <62942199+gwandubruce@users.noreply.github.com>
2021-02-05 20:14:28 +0200

    Update OwnIdsWebService.java

--- app/src/main/java/app/bandemic/strict/network/OwnIdsWebService.java
+++ app/src/main/java/app/bandemic/strict/network/OwnIdsWebService.java
@@ -1,11 +1,11 @@
-package app.bandemic.strict.network;
-
-import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.POST;
-
-public interface OwnIdsWebService {
-
-    @POST("/api/v1/app/result")
-    Call<OwnUUIDResponse> postOwnUUIDResponse(@Body OwnUUIDResponse ownUUIDResponse);
-}
+//package app.bandemic.strict.network;
+//
+//import retrofit2.Call;
+//import retrofit2.http.Body;
+//import retrofit2.http.POST;
+//
+//public interface OwnIdsWebService {
+//
+//    @POST("/api/v1/app/result")
+//    Call<OwnUUIDResponse> postOwnUUIDResponse(@Body OwnUUIDResponse ownUUIDResponse);
+//}
