3eb44693c2f7d682bfafe268db62e22b4515c60d
gwandubruce <62942199+gwandubruce@users.noreply.github.com>
2021-02-05 20:15:16 +0200

    Update InfectionIdsWebservice.java

--- app/src/main/java/app/bandemic/strict/network/InfectionIdsWebservice.java
+++ app/src/main/java/app/bandemic/strict/network/InfectionIdsWebservice.java
@@ -1,13 +1,13 @@
-package app.bandemic.strict.network;
-
-import retrofit2.Call;
-import retrofit2.http.GET;
-// This web service should be the one we focus on during testing as it gets the json with infected ids
-public interface InfectionIdsWebservice {
-
-    //"/api/v1/app/result"
-    //  .... "strict/items/infected_ids"  this is the replaced URL
-
-    @GET("/api/v1/app/results")
-    Call<InfectedUUIDResponse> getInfectedUUIDResponse();
-}
+//package app.bandemic.strict.network;
+//
+//import retrofit2.Call;
+//import retrofit2.http.GET;
+//// This web service should be the one we focus on during testing as it gets the json with infected ids
+//public interface InfectionIdsWebservice {
+//
+//    //"/api/v1/app/result"
+//    //  .... "strict/items/infected_ids"  this is the replaced URL
+//
+//    @GET("/api/v1/app/results")
+//    Call<InfectedUUIDResponse> getInfectedUUIDResponse();
+//}
